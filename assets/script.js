962371379ea785e257207e370a512f3c432fc06e
kat1eba3 <baek683@newschool.edu>
2023-10-09 20:46:59 -0400

    entry5

--- assets/script.js
+++ assets/script.js
@@ -24,3 +24,40 @@ document.addEventListener("DOMContentLoaded", function () {
     },
   
 ];
+
+<script>
+function setPlaylistImages() {
+        var playlistItems = document.querySelectorAll('.playlist-item');
+        var imageFolder = 'pics/'; // Replace with your folder path
+
+        // List of filenames
+        var imageFilenames = [
+            'DWG.jpg',
+            'supercut.jpg',
+            'smile.jpg',
+            'artemas.jpg',
+            'oakwood.jpg',
+            'sadmovies.jpg'
+            'busart.png'
+            'businessman.jpg'
+            'busturning.gif'
+            'joker.jpg'
+            'loudkid.jpg'
+            'meme.jpg'
+            'meme2.jpg'
+            'people.jpg'
+
+            // Add more filenames as needed
+        ];
+
+        // Loop through playlist items and set image src
+        playlistItems.forEach(function(item, index) {
+            var img = item.querySelector('.playlist-item-image');
+            var filename = imageFilenames[index];
+            img.src = imageFolder + filename;
+        });
+    }
+
+    // Call the function to set image source URLs
+    setPlaylistImages();
+</script>
