32c9deb8caa91d31fb8fb7d49a076183f87f597c
kat1eba3 <baek683@newschool.edu>
2023-10-17 02:45:50 -0400

    revise

--- assets/script.js
+++ assets/script.js
@@ -1,63 +1,63 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const buttons = document.querySelectorAll(".song-list button");
-    const audioPlayer = document.querySelector("#audio-player");
+// Get references to the audio player and audio source elements
+const audioPlayer = document.getElementById('audio-player');
+const audioSource = document.getElementById('audio-source');
 
-    buttons.forEach((button) => {
-        button.addEventListener("click", function () {
-            const songSrc = button.getAttribute("data-song");
-            audioPlayer.src = songSrc;
-            audioPlayer.load();
-            audioPlayer.play();
-        });
+// Get references to the buttons
+const buttons = document.querySelectorAll('.song-list button');
+
+// Add a click event listener to each button
+buttons.forEach((button) => {
+  button.addEventListener('click', function () {
+    // Get the data-song attribute of the clicked button
+    const songUrl = button.getAttribute('data-song');
+
+    // Set the audio source to the selected song
+    audioSource.src = songUrl;
+    albumCover.src = songs[index].imageURL;
+
+    // Load and play the audio
+    audioPlayer.load();
+    audioPlayer.play();
+  });
+});
+
+// Define album cover image URLs for each song
+const albumCovers = {
+    "MoneyTrees.mp3": "moneytreesalbum.jpg",
+    "oakwood.mp3": "oakwoodalbum.jpg",
+    "run.mp3": "runalbum.jpg",
+    "down.mp3": "downalbum.jpg",
+    "overandover.mp3": "overnover.jpg",
+};
+
+// Get references to HTML elements
+const albumCoverElement = document.getElementById("album-cover");
+const audioPlayerElement = document.getElementById("audio-player");
+const audioSourceElement = document.getElementById("audio-source");
+
+// Add a click event listener to each song button
+document.querySelectorAll(".song-list button").forEach(button => {
+    button.addEventListener("click", function () {
+        // Get the song URL from the button's data-song attribute
+        const songURL = button.getAttribute("data-song");
+        
+        // Set the src attribute of the album cover image
+        albumCoverElement.src = albumCovers[songURL];
+        
+        // Set the src attribute of the audio source
+        audioSourceElement.src = songURL;
+        
+        // Load and play the selected song
+        audioPlayerElement.load();
+        audioPlayerElement.play();
     });
 });
-  const songs = [
-    {
-        title: "Nice For What",
-        artist: "Artist 1",
-        audioUrl: "https://drive.google.com/file/d/1gzIkXoztYyJnLhieaw4Q5bdXkkjMdCvf/view?usp=sharing"
-    },
-    {
-        title: "Song 2",
-        artist: "Artist 2",
-        audioUrl: "song2.mp3"
-    },
-  
-];
 
-<script>
-function setPlaylistImages() {
-        var playlistItems = document.querySelectorAll('.playlist-item');
-        var imageFolder = 'pics/'; // Replace with your folder path
+// Wait for the document to be fully loaded
+document.addEventListener('DOMContentLoaded', function () {
+    // Get the audio element by its ID
+    var audio = document.getElementById('background-audio');
 
-        // List of filenames
-        var imageFilenames = [
-            'DWG.jpg',
-            'supercut.jpg',
-            'smile.jpg',
-            'artemas.jpg',
-            'oakwood.jpg',
-            'sadmovies.jpg'
-            'busart.png'
-            'businessman.jpg'
-            'busturning.gif'
-            'joker.jpg'
-            'loudkid.jpg'
-            'meme.jpg'
-            'meme2.jpg'
-            'people.jpg'
-
-            // Add more filenames as needed
-        ];
-
-        // Loop through playlist items and set image src
-        playlistItems.forEach(function(item, index) {
-            var img = item.querySelector('.playlist-item-image');
-            var filename = imageFilenames[index];
-            img.src = imageFolder + filename;
-        });
-    }
-
-    // Call the function to set image source URLs
-    setPlaylistImages();
-</script>
+    // Play the audio
+    audio.play();
+});
